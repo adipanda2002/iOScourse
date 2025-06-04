6f76997c1349ca463ca87ee6de0ec942d51469ea
Aditya Ghosh <adityaghosh2002@gmail.com>
2025-06-04 11:48:41 +0800

    add bmi calc

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Aditya Ghosh on 3/6/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
