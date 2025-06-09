d28cb38d943925fa2cca0706945b7a4407909d9e
Aditya Ghosh <adityaghosh2002@gmail.com>
2025-06-09 12:46:26 +0800

    add complete bytecoin project

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Aditya Ghosh on 9/6/25.
+//  Copyright © 2025 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
