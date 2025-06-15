e0bc1393b58ec064e3893f357597a81c93123ed6
Aditya Ghosh <adityaghosh2002@gmail.com>
2025-06-15 15:24:24 +0800

    complete messaging functionality

--- Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseFirestore
+import IQKeyboardManagerSwift
+import IQKeyboardToolbarManager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        let db = Firestore.firestore()
+        print(db)
+        IQKeyboardManager.shared.isEnabled = true
+        IQKeyboardToolbarManager.shared.isEnabled = false
+        IQKeyboardManager.shared.resignOnTouchOutside = true
         return true
     }
 
