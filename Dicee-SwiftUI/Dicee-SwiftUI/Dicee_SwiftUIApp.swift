b4f39b6b3b8741b300a22999d2d7c70f0e9b326b
Aditya Ghosh <adityaghosh2002@gmail.com>
2025-06-20 15:19:09 +0800

    Add state management to Dicee

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Aditya Ghosh on 20/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
